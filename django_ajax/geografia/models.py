14c6ce25cbeede7f8fb0da63264406c3f8b664f3
ljuan92 <lagojuanfrancisco@gmail.com>
2023-04-05 19:46:55 -0300

    Se carga la app en settings y se crea el modelo

--- django_ajax/geografia/models.py
+++ django_ajax/geografia/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Pais(models.Model):
+    nombre = models.CharField(max_length=50)
+    numero_habitantes = models.PositiveIntegerField()
+
+    def __str__(self):
+        return "{}".format(self.nombre)
+    
+    class Meta():
+        db_table = 'pais'
